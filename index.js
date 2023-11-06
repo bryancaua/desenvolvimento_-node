138f3bc9e4301f4801ddb45c44f015cf5d9fb056
bryan <bryanncaua@gmail.com>
2023-11-06 13:37:56 -0300

    arrumando a o commit errado que tinha feito

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const caminhoBase =  path.join(__dirname, "templates")
+
+//https://localhost:3000/usuarios/20
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
+//https://localhost:3000
+app.get('/', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/index.html`)
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000!")
+})
